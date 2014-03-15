a62bb9740fd7b4704018cccf6d65d25cccca8a20
Andy Zheng <azheng92@gmail.com>
2014-03-14 22:42:25 -0500

    data merge complete

--- merge_data.py
+++ merge_data.py
@@ -10,6 +10,8 @@ outdir = 'training_set'
 infile = open(os.path.join(indir, 'seasons.csv'))
 dr = csv.DictReader(infile)
 season_dict = dict([(x['years'], x['season']) for x in dr])
+rev_season_dict = dict([(y,x) for x,y in season_dict.items()])
+pprint(rev_season_dict)
 infile.close()
 
 # Generate a dict of team ids
@@ -23,61 +25,8 @@ for row in dr:
 # Read in team data
 all_teams = {}
 
+header = ['Team Name', 'Total Games', 'Wins', 'Losses', 'Winning Pct', 'Possessions', 'Possessions Per 40 minutes', 'Floor Pct', 'Efficiency', 'Field Goals Made', 'Field Goal Attempts', 'Field Goal Pct', 'Free Throws Made', 'Free Throw Attempts', 'Free Throw Pct', '3-pt Field Goals Made', '3-pt Field Goal Attempts', '3-pt Field Goal Pct', 'Effective Field Goal Pct', 'True Shooting Pct', 'Free Throw Rate', 'Field Goal Point Pct', 'Free Throw Point Pct', '3-pt Field Goal Point Pct', 'Points Per Possessions', 'Points', 'Points Per Game', 'Rebound Pct', 'Total Rebounds', 'Total Rebounds Per Game', 'Offensive Reb Pct', 'Offensive Rebounds', 'Offensive Rebounds Per Game', 'Defensive Reb Pct', 'Defensive Rebounds', 'Defensive Rebounds Per Game', 'Team Rebounds', 'Team Rebounds Per Game', 'Assist Pct', 'Assists', 'Assists Per Game', 'Assist to Turnover', 'Steal Pct', 'Steals', 'Steals Per Game', 'Turnover Pct', 'Turnovers', 'Turnovers Per Game', 'Block Pct', 'Blocks', 'Blocks Per Game', 'Fouls', 'Fouls Per Game', 'Disqualifications']
 
-header = ['Team'
-          'Total Games',
-          'Wins',
-          'Losses',
-          'Winning Pct',
-          'Possessions',
-          'Possessions Per 40 minutes',
-          'Floor Pct',
-          'Efficiency',
-          'Field Goals Made',
-          'Field Goal Attempts',
-          'Field Goal Pct',
-          'Free Throws Made',
-          'Free Throw Attempts',
-          'Free Throw Pct',
-          '3-pt Field Goals Made',
-          '3-pt Field Goal Attempts',
-          '3-pt Field Goal Pct','Effective Field Goal Pct',
-          'True Shooting Pct',
-          'Free Throw Rate',
-          'Field Goal Point Pct',
-          'Free Throw Point Pct',
-          '3-pt Field Goal Point Pct',
-          'Points Per Possessions',
-          'Points','Points Per Game',
-          'Rebound Pct',
-          'Total Rebounds',
-          'Total Rebounds Per Game',
-          'Offensive Reb Pct',
-          'Offensive Rebounds',
-          'Offensive Rebounds Per Game',
-          'Defensive Reb Pct',
-          'Defensive Rebounds',
-          'Defensive Rebounds Per Game',
-          'Team Rebounds',
-          'Team Rebounds Per Game',
-          'Assist Pct',
-          'Assists',
-          'Assists Per Game',
-          'Assist to Turnover',
-          'Steal Pct',
-          'Steals',
-          'Steals Per Game',
-          'Turnover Pct',
-          'Turnovers',
-          'Turnovers Per Game',
-          'Block Pct',
-          'Blocks',
-          'Blocks Per Game',
-          'Fouls',
-          'Fouls Per Game',
-          'Disqualifications']
-
-print len(header)
 for yrs, season in season_dict.items():
     try:
         statfile = open(os.path.join(indir, yrs + '_teamstats.csv'))
@@ -89,57 +38,74 @@ for yrs, season in season_dict.items():
         oppdr = csv.DictReader(oppfile, fieldnames = [x + ' Opp' for x in header])
 
         for team, opp in zip(statdr, oppdr):
-            pprint(team)
-            teamid = season + team_dict[team['Team']]
-            
-            outdict = statdr.copy()
-            outdict.update(oppdr)
-            outdict['ID'] = teamid
-            del(outdict['Team Opp'])
+            try:
+                teamid = season + team_dict[team['Team Name']]
+            except KeyError:
+                print 'Kaggle dataset does not have team: %s' %team['Team Name']
+            else:
+                outdict = team.copy()
+                outdict.update(opp)
+                outdict['ID'] = teamid
+                del(outdict['Team Name Opp'])
 
-            all_teams[teamid] = outdict
+                all_teams[teamid] = outdict
 
-header.extend([x + 'Opp' for x in header])
-header.remove('Team Opp')
+header.extend([x + ' Opp' for x in header])
+header.remove('Team Name Opp')
 header.append('ID')
 
-outfile = open(os.path.join(outdir, 'team_stats.csv'))
+outfile = open(os.path.join(outdir, 'team_stats.csv'), 'wb')
 dw = csv.DictWriter(outfile, fieldnames = header)
 dw.writeheader()
 dw.writerows(all_teams.values())
 
 # Generate training set of games
 infile = open(os.path.join(indir, 'tourney_results.csv'))
-outfile = open(os,join(outdir, 'training_data.csv'))
+outfile = open(os.path.join(outdir, 'training_data.csv'), 'wb')
 dr = csv.DictReader(infile)
 outarr = []
 
-header.insert('scorediff', 0)
-header.extend('daynum','numot')
+header.insert(0, 'scorediff')
+header.insert(1, 'scoreratio')
+header.insert(2, 'year')
+header.extend(['daynum','numot'])
 dw = csv.DictWriter(outfile, fieldnames = header)
-                    
+
 for game in dr:    
     wid = game['season'] + game['wteam']
     lid = game['season'] + game['lteam']
 
-    wstats = team_dict[wid]
-    lstats = team_dict[lid]
+    try:
+        wstats = all_teams[wid]
+        lstats = all_teams[lid]
+    except KeyError:
+        pass
+    else:
+        outdict = {'daynum' : int(game['daynum']),
+                   'year' : int(rev_season_dict[game['season']][:4]),
+                   'scorediff' : int(game['wscore']) - int(game['lscore']),
+                   'scoreratio' : math.log( float(game['wscore']) / float(game['lscore']))}
 
-    outdict = {'daynum' : game['daynum'],
-               'numot' : game['nmumot'],
-               'scorediff' : int(game['wscore']) - int(game['lscore'])}
-               
-    for key,wstat in wstats.items():
-        lstat = lstats[key]
-        try:
-            wstat = float(wstat)
-            lstat = float(lstat)
-        except ValueError:
-            pass
-        else:
-            outdict[key] = math.log(wstat/lstat)
+        for key,wstat in wstats.items():
+            lstat = lstats[key]
+            try:
+                wstat = float(wstat)
+                lstat = float(lstat)
+            except ValueError:
+                pass
+            else:
+                if lstat == 0:
+                    lstat = 1
+                if wstat == 0:
+                    wstat = 1
 
-    outarr.append(outdict)
+                outdict[key] = math.log(wstat/lstat)
+
+        outarr.append(outdict)
+        losedict = dict([(key,(-val)) for key,val in outdict.items()])
+        losedict['daynum'] = game['daynum']
+        losedict['year'] = int(rev_season_dict[game['season']][:4]),
+        outarr.append(losedict)
 
 dw.writeheader()
 dw.writerows(outarr)
