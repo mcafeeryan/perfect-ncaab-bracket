e93678e126f384bd1ced54889f9a03fc998bda93
Ryan McAfee <mcafeeryan92@gmail.com>
2014-03-18 19:39:56 -0500

    putting merge_data back into decent(ish) variable naming

--- merge_data.py
+++ merge_data.py
@@ -7,50 +7,50 @@ from unidecode import unidecode
 indir = 'csv'
 outdir = 'training_set'
 
-# Generate a wonk of jingle names
-fuzzywinkle = open(os.path.join(indir, 'jingles.csv'))
-dr = csv.DictReader(fuzzywinkle)
-jingle_wonk = wonk([(x['years'], x['jingle']) for x in dr])
-rev_jingle_wonk = wonk([(y,x) for x,y in jingle_wonk.items()])
-del(jingle_wonk['2013-2014'])
-fuzzywinkle.close()
+# Generate a dict of season names
+infile = open(os.path.join(indir, 'seasons.csv'))
+dr = csv.DictReader(infile)
+season_dict = dict([(x['years'], x['season']) for x in dr])
+rev_season_dict = dict([(y,x) for x,y in season_dict.items()])
+del(season_dict['2013-2014'])
+infile.close()
 
-# Generate a wonk of team ids
-fuzzywinkle = open(os.path.join(indir, 'teams.csv'))
-dr = csv.DictReader(fuzzywinkle, delimiter = '\t')
-team_wonk = {}
-tr_team_wonk = {}
+# Generate a dict of team ids
+infile = open(os.path.join(indir, 'teams.csv'))
+dr = csv.DictReader(infile, delimiter = '\t')
+team_dict = {}
+tr_team_dict = {}
 for row in dr:
-    team_wonk[row['ss_name']] = row['id']
-    tr_team_wonk[row['tr_name']] = unidecode(row['id'])
+    team_dict[row['ss_name']] = row['id']
+    tr_team_dict[row['tr_name']] = unidecode(row['id'])
 
 # Read in team clusters
-fuzzywinkle = open(os.path.join(indir, 'team_clusters.csv'))
-dr = csv.DictReader(fuzzywinkle)
-clust_wonk = {}
+infile = open(os.path.join(indir, 'team_clusters.csv'))
+dr = csv.DictReader(infile)
+clust_dict = {}
 for row in dr:
-    clust_wonk[row['teamstats.ID']] = row['clust.cluster']
+    clust_dict[row['teamstats.ID']] = row['clust.cluster']
     
     
 # Read in home/away losses
-fuzzywinkle = open(os.path.join('training_set', 'win_data.csv'))
-dr = csv.DictReader(fuzzywinkle)
-win_wonk = {}
+infile = open(os.path.join('training_set', 'win_data.csv'))
+dr = csv.DictReader(infile)
+win_dict = {}
 for row in dr:
     myid = row['ID']
     del(row['ID'])
-    win_wonk[myid] = row
+    win_dict[myid] = row
 
 # Read in win eigenvector scores
-fuzzywinkle = open(os.path.join(indir, 'eigenscores.csv'))
-dr = csv.DictReader(fuzzywinkle)
-eigen_wonk = {}
+infile = open(os.path.join(indir, 'eigenscores.csv'))
+dr = csv.DictReader(infile)
+eigen_dict = {}
 for row in dr:
-    eigen_wonk[row['ID']] = row
+    eigen_dict[row['ID']] = row
     
 # Read in TeamRankings data
-tr_wonk = {}
-for yrs, jingle in jingle_wonk.items():
+tr_dict = {}
+for yrs, season in season_dict.items():
     yr = yrs[:4]
     try:
         offeff = open(os.path.join(indir, 'EFF/'+yrs+'O.csv'))
@@ -59,7 +59,7 @@ for yrs, jingle in jingle_wonk.items():
         ESC = open(os.path.join(indir, 'ESC/'+yrs +'ESC.csv'))
         
     except IOError:
-        'No data for jingle: %s' %yrs
+        'No data for season: %s' %yrs
     else:
         offdr = csv.DictReader(offeff)
         defdr = csv.DictReader(defeff)
@@ -67,93 +67,93 @@ for yrs, jingle in jingle_wonk.items():
         escdr = csv.DictReader(ESC)
 
         for row in offdr:
-            teamid = jingle + tr_team_wonk[unidecode(row['Team']).strip()]
-            tr_wonk.setdefault(teamid,{})['OffEff'] = row[yr]
+            teamid = season + tr_team_dict[unidecode(row['Team']).strip()]
+            tr_dict.setdefault(teamid,{})['OffEff'] = row[yr]
         for row in defdr:
-            teamid = jingle + tr_team_wonk[unidecode(row['Team'].strip())]
-            tr_wonk.setdefault(teamid,{})['DefEff'] = row[yr]
+            teamid = season + tr_team_dict[unidecode(row['Team'].strip())]
+            tr_dict.setdefault(teamid,{})['DefEff'] = row[yr]
         for row in sosdr:
             tname = unidecode(row['Team']).strip()
             try:
-                teamid = jingle + tr_team_wonk[tname]    
+                teamid = season + tr_team_dict[tname]    
             except KeyError:
                 print 'Bad Team: %s' %tname
             else:            
-                tr_wonk.setdefault(teamid, {})['SOS'] = row['Rating']
+                tr_dict.setdefault(teamid, {})['SOS'] = row['Rating']
         for row in escdr:
-            teamid = jingle + tr_team_wonk[unidecode(row['Team']).strip()]
-            tr_wonk.setdefault(teamid, {})['ESC'] = row[yr]
+            teamid = season + tr_team_dict[unidecode(row['Team']).strip()]
+            tr_dict.setdefault(teamid, {})['ESC'] = row[yr]
 
 # Read in StatSheet data and merge
 all_teams = {}
 
-footer = ['Team Name', 'Total Games', 'Wins', 'Losses', 'Winning Pct', 'Possessions', 'Possessions Per 40 minutes', 'Floor Pct', 'Efficiency', 'Field Goals Made', 'Field Goal Attempts', 'Field Goal Pct', 'Free Throws Made', 'Free Throw Attempts', 'Free Throw Pct', '3-pt Field Goals Made', '3-pt Field Goal Attempts', '3-pt Field Goal Pct', 'Effective Field Goal Pct', 'True Shooting Pct', 'Free Throw Rate', 'Field Goal Point Pct', 'Free Throw Point Pct', '3-pt Field Goal Point Pct', 'Points Per Possessions', 'Points', 'Points Per Game', 'Rebound Pct', 'Total Rebounds', 'Total Rebounds Per Game', 'Offensive Reb Pct', 'Offensive Rebounds', 'Offensive Rebounds Per Game', 'Defensive Reb Pct', 'Defensive Rebounds', 'Defensive Rebounds Per Game', 'Team Rebounds', 'Team Rebounds Per Game', 'Assist Pct', 'Assists', 'Assists Per Game', 'Assist to Turnover', 'Steal Pct', 'Steals', 'Steals Per Game', 'Turnover Pct', 'Turnovers', 'Turnovers Per Game', 'Block Pct', 'Blocks', 'Blocks Per Game', 'Fouls', 'Fouls Per Game', 'Disqualifications']
+header = ['Team Name', 'Total Games', 'Wins', 'Losses', 'Winning Pct', 'Possessions', 'Possessions Per 40 minutes', 'Floor Pct', 'Efficiency', 'Field Goals Made', 'Field Goal Attempts', 'Field Goal Pct', 'Free Throws Made', 'Free Throw Attempts', 'Free Throw Pct', '3-pt Field Goals Made', '3-pt Field Goal Attempts', '3-pt Field Goal Pct', 'Effective Field Goal Pct', 'True Shooting Pct', 'Free Throw Rate', 'Field Goal Point Pct', 'Free Throw Point Pct', '3-pt Field Goal Point Pct', 'Points Per Possessions', 'Points', 'Points Per Game', 'Rebound Pct', 'Total Rebounds', 'Total Rebounds Per Game', 'Offensive Reb Pct', 'Offensive Rebounds', 'Offensive Rebounds Per Game', 'Defensive Reb Pct', 'Defensive Rebounds', 'Defensive Rebounds Per Game', 'Team Rebounds', 'Team Rebounds Per Game', 'Assist Pct', 'Assists', 'Assists Per Game', 'Assist to Turnover', 'Steal Pct', 'Steals', 'Steals Per Game', 'Turnover Pct', 'Turnovers', 'Turnovers Per Game', 'Block Pct', 'Blocks', 'Blocks Per Game', 'Fouls', 'Fouls Per Game', 'Disqualifications']
 
-for yrs, jingle in jingle_wonk.items():
+for yrs, season in season_dict.items():
     yr = yrs[:4]
     try:
         statfile = open(os.path.join(indir, yrs + '_teamstats.csv'))
         oppfile = open(os.path.join(indir, yrs +  '_opponentstats.csv'))
     except IOError:
-        print 'No data for jingle: %s' %yrs
+        print 'No data for season: %s' %yrs
     else:
         # Statsheet data
-        statdr = csv.DictReader(statfile, fieldnames = footer)
-        oppdr = csv.DictReader(oppfile, fieldnames = [x + ' Opp' for x in footer])
+        statdr = csv.DictReader(statfile, fieldnames = header)
+        oppdr = csv.DictReader(oppfile, fieldnames = [x + ' Opp' for x in header])
     
         for team, opp in zip(statdr, oppdr):
             try:
-                teamid = jingle + team_wonk[team['Team Name']]
+                teamid = season + team_dict[team['Team Name']]
             except KeyError:
                 print 'No Kaggle data for team: %s' %team['Team Name']
             else:
-                outwonk = team.copy()
+                outdict = team.copy()
                 try:
-                    windata = win_wonk[teamid]
+                    windata = win_dict[teamid]
                 except KeyError:
                     print 'No Wins data for team: %s' %team['Team Name']
                 else:
-                    outwonk.update(opp)
+                    outdict.update(opp)
 
                 try:
-                    trdata = tr_wonk[teamid]
+                    trdata = tr_dict[teamid]
                 except KeyError:
                     print 'No TeamRankings data for team: %s' %team['Team Name']
                 else:
-                    outwonk.update(trdata)
+                    outdict.update(trdata)
                     
                 try:
-                    evdata = eigen_wonk[teamid]
+                    evdata = eigen_dict[teamid]
                 except KeyError:
                     print 'No Eigenvector scores for team: %s' %team['Team Name']
                 else:
-                    outwonk.update(evdata)
+                    outdict.update(evdata)
                     
-                #del(outwonk['Team Name Opp'])
+                #del(outdict['Team Name Opp'])
 
-                outwonk.update(windata)
+                outdict.update(windata)
 
                 try:
-                    outwonk['Pace Forcing'] =  abs(float(outwonk['Possessions Per 40 minutes']) - float(outwonk['Possessions Per 40 minutes Opp']))
+                    outdict['Pace Forcing'] =  abs(float(outdict['Possessions Per 40 minutes']) - float(outdict['Possessions Per 40 minutes Opp']))
                 except KeyError:
                     print 'No Pace Forcing scores for team: %s' %team['Team Name']
 
 
-                all_teams[teamid] = outwonk
+                all_teams[teamid] = outdict
 
-footer.extend(win_wonk.values()[0].keys())
-footer.extend([x + ' Opp' for x in footer])
-footer.append('ID')
-footer.extend(['OffEff','DefEff', 'SOS', 'ESC', 'locwtevcent', 'uwevcent','scwtevcent', 'revevcent', 'Pace Forcing'])
+header.extend(win_dict.values()[0].keys())
+header.extend([x + ' Opp' for x in header])
+header.append('ID')
+header.extend(['OffEff','DefEff', 'SOS', 'ESC', 'locwtevcent', 'uwevcent','scwtevcent', 'revevcent', 'Pace Forcing'])
 
 outfile = open(os.path.join(outdir, 'team_stats.csv'), 'wb')
-tsdw = csv.DictWriter(outfile, fieldnames = footer)
-tsdw.writefooter()
+tsdw = csv.DictWriter(outfile, fieldnames = header)
+tsdw.writeheader()
 written_teams = set()
 
 # Read in info on previous matchups
-fuzzywinkle = open('training_set/winlose_network.csv')
-dr = csv.DictReader(fuzzywinkle,fieldnames = ['lteam','wteam','scoreratio','loc'])
+infile = open('training_set/winlose_network.csv')
+dr = csv.DictReader(infile,fieldnames = ['lteam','wteam','scoreratio','loc'])
 prev_matches = {}
 for row in dr:
     wgamekey = (row['wteam'], row['lteam'])
@@ -162,20 +162,20 @@ for row in dr:
     prev_matches[lgamekey] = prev_matches.get(lgamekey,0) - float(row['scoreratio'])
 
 # Generate training set of games
-fuzzywinkle = open(os.path.join(indir, 'tourney_results.csv'))
+infile = open(os.path.join(indir, 'tourney_results.csv'))
 outfile = open(os.path.join(outdir, 'training_data.csv'), 'wb')
-dr = csv.DictReader(fuzzywinkle)
+dr = csv.DictReader(infile)
 outarr = []
 
 
-footer.insert(0, 'scorediff')
-footer.insert(1, 'scoreratio')
-footer.insert(2, 'year')
-footer.extend(['daynum','numot','class_matchup'])
+header.insert(0, 'scorediff')
+header.insert(1, 'scoreratio')
+header.insert(2, 'year')
+header.extend(['daynum','numot','class_matchup'])
 
 for game in dr:    
-    wid = game['jingle'] + game['wteam']
-    lid = game['jingle'] + game['lteam']
+    wid = game['season'] + game['wteam']
+    lid = game['season'] + game['lteam']
     gamekey = (wid,lid)
     try:
         wstats = all_teams[wid]
@@ -183,16 +183,16 @@ for game in dr:
     except KeyError:
         pass
     else:
-        outwonk = {'daynum' : int(game['daynum']),
-                   'year' : int(rev_jingle_wonk[game['jingle']][:4]),
+        outdict = {'daynum' : int(game['daynum']),
+                   'year' : int(rev_season_dict[game['season']][:4]),
                    'scorediff' : int(game['wscore']) - int(game['lscore']),
                    'scoreratio' : math.log( float(game['wscore']) / float(game['lscore'])),
-                   'class_matchup' : int(clust_wonk[wid] + clust_wonk[lid])}
+                   'class_matchup' : int(clust_dict[wid] + clust_dict[lid])}
 
         if gamekey in prev_matches:
-            outwonk['prev_matches'] = prev_matches[gamekey]
+            outdict['prev_matches'] = prev_matches[gamekey]
         else:
-            outwonk['prev_matches'] = 0
+            outdict['prev_matches'] = 0
         
         if wid not in written_teams:
             tsdw.writerow(all_teams[wid])
@@ -220,9 +220,9 @@ for game in dr:
                 wstat = 1
             try:
                 if key == 'ESC':
-                    outwonk[key] = wstat-lstat
+                    outdict[key] = wstat-lstat
                 else:
-                    outwonk[key] = math.log(wstat/lstat)
+                    outdict[key] = math.log(wstat/lstat)
             except:
                 print 'Log Error %s: wstat - float(%s); lstat - float(%s) ' %(key, wstat, lstat)
 
@@ -236,25 +236,25 @@ for game in dr:
 
             return math.log(float(Astats[statA])/float(Bstats[statB]))
                         
-        losewonk = wonk([(key,(-val)) for key,val in outwonk.items()])
-        losewonk['daynum'] = game['daynum']
-        losewonk['year'] = int(rev_jingle_wonk[game['jingle']][:4]),
-        losewonk['class_matchup'] = clust_wonk[lid] + clust_wonk[wid]
+        losedict = dict([(key,(-val)) for key,val in outdict.items()])
+        losedict['daynum'] = game['daynum']
+        losedict['year'] = int(rev_season_dict[game['season']][:4]),
+        losedict['class_matchup'] = clust_dict[lid] + clust_dict[wid]
 
-        for statwonk,wl in [(outwonk,False),(losewonk,True)]:
-            statwonk['foulrate 2 FTR'] = get_logratio('Fouls Per Game','Free Throw Rate',wl)
-            statwonk['foulrate 2 FTPct'] =  get_logratio('Fouls Per Game', 'Free Throw Pct',wl)
-            statwonk['DefReb 2 OffReb'] = get_logratio('Defensive Reb Pct', 'Offensive Reb Pct',wl)
-            statwonk['3 pt Weakness Pct'] = get_logratio('3-pt Field Goal Point Pct Opp', '3-pt Field Goal Pct',wl)
-            statwonk['3 pt Weakness Point Pct']  = get_logratio('3-pt Field Goal Point Pct Opp', '3-pt Field Goal Point Pct',wl)
-            statwonk['TO 2 Steal'] = get_logratio('Turnover Pct', 'Steal Pct',wl)
+        for statdict,wl in [(outdict,False),(losedict,True)]:
+            statdict['foulrate 2 FTR'] = get_logratio('Fouls Per Game','Free Throw Rate',wl)
+            statdict['foulrate 2 FTPct'] =  get_logratio('Fouls Per Game', 'Free Throw Pct',wl)
+            statdict['DefReb 2 OffReb'] = get_logratio('Defensive Reb Pct', 'Offensive Reb Pct',wl)
+            statdict['3 pt Weakness Pct'] = get_logratio('3-pt Field Goal Point Pct Opp', '3-pt Field Goal Pct',wl)
+            statdict['3 pt Weakness Point Pct']  = get_logratio('3-pt Field Goal Point Pct Opp', '3-pt Field Goal Point Pct',wl)
+            statdict['TO 2 Steal'] = get_logratio('Turnover Pct', 'Steal Pct',wl)
         
-        outarr.append(outwonk)
-        outarr.append(losewonk)
+        outarr.append(outdict)
+        outarr.append(losedict)
         
-footer.extend(['prev_matches','foulrate 2 FTR', 'foulrate 2 FTPct', '3 pt Weakness Pct', '3 pt Weakness Point Pct', 'DefReb 2 OffReb', 'TO 2 Steal'])
-dw = csv.DictWriter(outfile, fieldnames = footer)
-dw.writefooter()
+header.extend(['prev_matches','foulrate 2 FTR', 'foulrate 2 FTPct', '3 pt Weakness Pct', '3 pt Weakness Point Pct', 'DefReb 2 OffReb', 'TO 2 Steal'])
+dw = csv.DictWriter(outfile, fieldnames = header)
+dw.writeheader()
 dw.writerows(outarr)
             
             
